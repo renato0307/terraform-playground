97cb320fdcd609ce707f99054e342c08ca92ed4b
Renato Torres <renato.torres@outsystems.com>
2021-01-20 16:52:46 +0000

    feat: add deployment of lambda

---
+++ infra/tf_lambdas/provider.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  token = var.AWS_SESSION_TOKEN
+
+  assume_role {
+    role_arn    = var.AWS_ROLE_ARN
+  }
+
+}
+
+provider "archive" {}
+
+provider "null" {}
+
+provider "random" {}
